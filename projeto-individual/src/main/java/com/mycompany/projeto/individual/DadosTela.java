9d40b4a71b5be90fda7e0b512c65fd09fc3d2501
fernandofialho <61751898+fernandofialho@users.noreply.github.com>
2020-09-05 11:16:32 -0300

    Add files via upload

---
+++ projeto-individual/src/main/java/com/mycompany/projeto/individual/DadosTela.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.projeto.individual;
+
+/**
+ *
+ * @author Aluno
+ */
+public class DadosTela {
+    Integer cont = 0;
+    Double random;
+    Double somas = 0.0;
+    Double media = 0.0;
+    Double min = 0.0;
+    Double max = 0.0;
+}
